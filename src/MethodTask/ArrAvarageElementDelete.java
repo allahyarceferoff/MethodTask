362c4c152ff5c5a5dbba7c5f2268854b139eeab1
allahyarceferoff <95705019+allahyarceferoff@users.noreply.github.com>
2024-04-23 20:12:27 +0400

    Update ArrAvarageElementDelete.java

--- src/MethodTask/ArrAvarageElementDelete.java
+++ src/MethodTask/ArrAvarageElementDelete.java
@@ -1,2 +1,31 @@
-package MethodTask;public class ArrAvarageElementDelete {
+package MethodTask;
+
+public class ArrAvarageElementDelete {
+    public static void main(String[] args) {
+        int[] intArr = {1,2,3,4,5,6,7,8};
+        int[] myArr=averageElementDeleteArr(intArr,3);
+        for (int i=0;i<myArr.length;i++){
+            System.out.println(myArr[i]);
+        }
+        System.out.println();
+
+
+    }
+
+
+public static int[] averageElementDeleteArr(int[] arr,int element){
+    int[] newArr=new int[arr.length-1];
+    for (int i=0,k=0;i<arr.length;i++){
+        if(element!=arr[i] ){
+            newArr[k]=arr[i];
+            k++;
+        }
+
+
+    }
+    return newArr;
 }
+
+
+}
+
